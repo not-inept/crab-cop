40257adb915332ad807ccc97086dccb7b8333382
not-inept <mason@notinept.me>
2018-08-01 23:51:18 -0400

    Added start of registration system.

--- crabcop.py
+++ crabcop.py
@@ -2,9 +2,20 @@ import discord
 import asyncio
 import yaml
 
+registrants = {}
+channels = {}
+
+
+class Registrant:
+    def Registrant(self, token):
+        self.token = token
+
 def totalVocab(srs):
     return srs.apprentice.vocabulary + srs.burned.vocabulary + srs.enlighten.vocabulary + srs.guru.vocabulary + srs.master.vocabulary
-    
+
+def loadChannels():
+    client.get_channel
+    client.get_user_info  
 
 from crabigator.wanikani import WaniKani
 
@@ -21,18 +32,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith("!wani"):
+    if message.content.startswith("!wani"):
         response = ""
         for user in config["users"]:
             wc = WaniKani(user["key"])
@@ -47,5 +47,37 @@ async def on_message(message):
             response += "\n\tlessons available: %d" % stud.lessons_available
             response += "\n\treviews available: %d" % stud.reviews_available
         await client.send_message(message.channel, response)
+    elif message.content.startswith("!register"):
+        response = await client.send_message(message.channel, 
+        "Please finish your registration by responding to the private message I'm about to send you.")
+        registrants[message.author.id] = (message.author, message.channel, response)
+        await client.send_message(message.author, "Please respond with `!token <api v1 token>` to add your WaniKani token to my list. You can your token at: https://www.wanikani.com/settings/account")
+        await client.edit_message(response, "Please finish your registration by responding to the private message I just sent you.")
+    elif message.content.startswith("!token") and message.channel.is_private and message.author.id in registrants:
+        # validate argument exists (api token)
+        split_content = message.content.split(" ")
+        if len(split_content) != 2:
+            await client.send_message(message.channel, "Make you sure you pass a token like: `!token <api v1 token>`")
+            return
+        token = split_content[1]
+
+        # validate api token works
+        response = await client.send_message(message.channel, "Thanks, I'm storing your information...")
+        registrant = registrants[message.author.id]
+        channel = channels.setdefault(registrant[1].id, {})
+        # save user data for later
+
+        channel[registrant[0]] = (registrant[0], token)
+
+        # add channel to global channels list?
+        await client.edit_message(response, "You're all registered!")
+        await client.edit_message(registrant[2], "Thanks for registering!")
+
+    # Add register method-- add a user to the list of users to track, store:
+    # -- unique discord ID
+    # -- channel registered in
+    # -- api token
+    
+    # Add a thread that checks automatically and says nice things if stuff improves
 
 client.run(config["token"])
